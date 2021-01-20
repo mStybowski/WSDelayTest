1f1101c2781b3c1392f74489c6ab5f9326c69c04
mbool <mstybowski@gmail.com>
2021-01-20 17:51:24 +0100

    csv

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 const app = require("express")()
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 const expressWs = require('express-ws')(app)
 
 let port = 8888;
 
+function generateCSV(array){
+
+    let myArrayOfStrings = array.split(',');
+    console.log(myArrayOfStrings);
+
+    let arrayOfValues = myArrayOfStrings.map(function(x) {
+        return {value:parseInt(x)}
+    });
+
+    console.log(arrayOfValues);
+
+    const csvWriter = createCsvWriter({
+        path: 'out.csv',
+        header: [
+            {id: 'value', title: 'Value'}
+        ]
+    });
+
+
+    csvWriter
+        .writeRecords(arrayOfValues)
+        .then(()=> console.log('The CSV file was written successfully'));
+}
+
 app.ws('/echo', function(ws, req) {
     ws.on('message', function(msg) {
         console.log(msg)
+        if(msg.length > 100){
+            generateCSV(msg)
+        }
         ws.send(msg);
     });
 });
